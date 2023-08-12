bcde07c0e514804e80666dd3c8da14af31cf3e89
Caio Moizés Santos <engcaiomoizes@gmail.com>
2023-08-12 03:39:54 -0300

    first commit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/scss/app.scss'])
+    <!-- @vite(['resources/scss/app.scss']) -->
+    <link rel="preload" as="style" href="http://cukamonga-ecommerce.vercel.app/build/assets/app-0198a505.css" /><link rel="stylesheet" href="http://cukamonga-ecommerce.vercel.app/build/assets/app-0198a505.css" />
 </head>
 <body>
 
