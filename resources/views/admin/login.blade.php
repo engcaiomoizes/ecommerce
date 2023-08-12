d82762197ef150dee16cdee0d289ba7043e873c3
Caio Moizés Santos <engcaiomoizes@gmail.com>
2023-08-11 22:08:32 -0300

    first commit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,5 +11,4 @@
     <h1>Faça login!</h1>
 
 </body>
-</html> -->
-<h1>Faça login!</h1>
+</html>
