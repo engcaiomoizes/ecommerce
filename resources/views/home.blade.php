0c99bd6c62775361f7459474eeacbb4d9d48722b
Caio Moizés Santos <engcaiomoizes@gmail.com>
2023-08-12 03:50:07 -0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/scss/app.scss'])
-</head>
-<body>
-    <h1>Teste de texto</h1>
-</body>
-</html>
+@vite(['resources/scss/app.scss'])
+<h1>Teste de texto</h1>
